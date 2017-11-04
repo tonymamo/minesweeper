1823de57bb4e5ff3322a174271937302f1f24137
Tony Mamo <tonymamo@flashrecruit.com>
2017-11-04 09:54:29 -0700

    snapshot tests added

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Board from './Board';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+
+import configureStore from '../../redux/configureStore';
+import BoardContainer from './BoardContainer';
+
+const store = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Board />, div);
+  ReactDOM.render(<Provider store={store}><BoardContainer /></Provider>, div);
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Provider store={store}><BoardContainer /></Provider>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/components/Cell/Cell.test.js
+++ src/components/Cell/Cell.test.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 
-import configureStore from '../redux/configureStore';
-import AppContainer from './AppContainer';
+import configureStore from '../../redux/configureStore';
+import Cell from './Cell';
 
 const store = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><AppContainer /></Provider>, div);
+  ReactDOM.render(<Provider store={store}><Cell /></Provider>, div);
 });
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Provider store={store}><AppContainer /></Provider>
+    <Provider store={store}><Cell /></Provider>
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
